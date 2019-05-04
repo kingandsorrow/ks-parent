fb95bc18bf890cac3ec3b42a7c80107bd9a5b5b9
kingandsorrow <kingandsorrow@gmail.com>
2019-05-04 21:56:07 +0800

    ks-footer.css

--- ks-vue/style/js/include.js
+++ ks-vue/style/js/include.js
@@ -13,7 +13,7 @@ var appSetting = {
     include: [
         // meta
         '<meta name="renderer" content="webkit">',
-        '<meta content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no" name="viewport">',
+        '<meta name="viewport" content="width=device-width, initial-scale=0.5, maximum-scale=1 minimum-scale=0.5 user-scalable=no" >',
         '<meta content="yes" name="apple-mobile-web-app-capable">',
         '<meta content="black" name="apple-mobile-web-app-status-bar-style">',
         '<meta name="format-detection" content="telephone=no" />',
